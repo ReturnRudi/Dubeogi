06ad167e2fc9595759a1f30c4951722ec0875f29
ReturnRudi <qnvr31p@naver.com>
2023-05-01 20:23:29 +0900

    임시 길찾기 기능 구현

---
+++ lib/component/draw_line.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:Dubeogi/algorithm/astar.dart';
+
+class LinePainter extends CustomPainter {
+  // 지도에 선을 그리는 클래스
+  final ImageInfo imageInfo;
+  final List<Offset> startPoints;
+  final List<Offset> endPoints;
+
+  LinePainter({
+    required this.imageInfo,
+    required this.startPoints,
+    required this.endPoints,
+  });
+
+  void erase(){
+    startPoints.clear();
+    endPoints.clear();
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final scaleX = size.width / imageInfo.image.width;
+    final scaleY = size.height / imageInfo.image.height;
+    print(
+        'size.width: ${size.width}, imageInfo.image.width: ${imageInfo.image.width}');
+    print(
+        'size.height: ${size.height}, imageInfo.image.height: ${imageInfo.image.height}');
+
+    print('scaleX: $scaleX, scaleY: $scaleY');
+
+    final paint = Paint()
+      ..color = Colors.red
+      ..strokeWidth = 1
+      ..style = PaintingStyle.stroke;
+
+    for (int i = 0; i < startPoints.length; i++) {
+      canvas.drawLine(
+        startPoints[i].scale(scaleX, scaleY),
+        endPoints[i].scale(scaleX, scaleY),
+        paint,
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
