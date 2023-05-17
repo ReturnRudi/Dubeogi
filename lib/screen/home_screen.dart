1295cf59d0acde1d0ae6872766157cb3fcce5316
ReturnRudi <qnvr31p@naver.com>
2023-05-17 14:19:07 +0900

    편의 시설 코드 누락 수정

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -746,6 +746,186 @@ class _HomeScreenState extends State<HomeScreen> {
                                             ),
                                           ),
                                         ),
+                                        if (_vendingvisibility)
+                                          Positioned(
+                                            left: 971 * scale_offset,
+                                            top: 2479 * scale_offset,
+                                            child: Image.asset(
+                                              vendingPath,
+                                              scale: 1 / (scale_offset / 16),
+                                            ),
+                                          ),
+                                        if (_vendingvisibility)
+                                          Positioned(
+                                            left: 725 * scale_offset,
+                                            top: 2770 * scale_offset,
+                                            child: Image.asset(
+                                              vendingPath,
+                                              scale: 1 / (scale_offset / 16),
+                                            ),
+                                          ),
+                                        if (_vendingvisibility)
+                                          Positioned(
+                                            left: 2360 * scale_offset,
+                                            top: 2940 * scale_offset,
+                                            child: Image.asset(
+                                              vendingPath,
+                                              scale: 1 / (scale_offset / 16),
+                                            ),
+                                          ),
+                                        if (_vendingvisibility)
+                                          Positioned(
+                                            left: 825 * scale_offset,
+                                            top: 2022 * scale_offset,
+                                            child: Image.asset(
+                                              vendingPath,
+                                              scale: 1 / (scale_offset / 16),
+                                            ),
+                                          ),
+                                        if (_showervisibility)
+                                          Positioned(
+                                            left: 927 * scale_offset,
+                                            top: 2537 * scale_offset,
+                                            child: Image.asset(
+                                              showerPath,
+                                              scale: 1 / (scale_offset / 16),
+                                            ),
+                                          ),
+                                        if (_showervisibility)
+                                          Positioned(
+                                            left: 1650 * scale_offset,
+                                            top: 2616 * scale_offset,
+                                            child: Image.asset(
+                                              showerPath,
+                                              scale: 1 / (scale_offset / 16),
+                                            ),
+                                          ),
+                                        if (_storevisibility)
+                                          Positioned(
+                                            left: 865 * scale_offset,
+                                            top: 2279 * scale_offset,
+                                            child: Image.asset(
+                                              storePath,
+                                              scale: 1 / (scale_offset / 16),
+                                            ),
+                                          ),
+                                        if (_storevisibility)
+                                          Positioned(
+                                            left: 694 * scale_offset,
+                                            top: 2596 * scale_offset,
+                                            child: Image.asset(
+                                              storePath,
+                                              scale: 1 / (scale_offset / 16),
+                                            ),
+                                          ),
+                                        if (_storevisibility)
+                                          Positioned(
+                                            left: 1028 * scale_offset,
+                                            top: 2736 * scale_offset,
+                                            child: Image.asset(
+                                              storePath,
+                                              scale: 1 / (scale_offset / 16),
+                                            ),
+                                          ),
+                                        if (_storevisibility)
+                                          Positioned(
+                                            left: 1123 * scale_offset,
+                                            top: 3168 * scale_offset,
+                                            child: Image.asset(
+                                              storePath,
+                                              scale: 1 / (scale_offset / 16),
+                                            ),
+                                          ),
+                                        if (_storevisibility)
+                                          Positioned(
+                                            left: 1955 * scale_offset,
+                                            top: 2891 * scale_offset,
+                                            child: Image.asset(
+                                              storePath,
+                                              scale: 1 / (scale_offset / 16),
+                                            ),
+                                          ),
+                                        if (_storevisibility)
+                                          Positioned(
+                                            left: 1107 * scale_offset,
+                                            top: 1719 * scale_offset,
+                                            child: Image.asset(
+                                              storePath,
+                                              scale: 1 / (scale_offset / 16),
+                                            ),
+                                          ),
+                                        if (_printervisibility)
+                                          Positioned(
+                                            left: 854 * scale_offset,
+                                            top: 2696 * scale_offset,
+                                            child: Image.asset(
+                                              printerPath,
+                                              scale: 1 / (scale_offset / 16),
+                                            ),
+                                          ),
+                                        if (_printervisibility)
+                                          Positioned(
+                                            left: 936 * scale_offset,
+                                            top: 2416 * scale_offset,
+                                            child: Image.asset(
+                                              printerPath,
+                                              scale: 1 / (scale_offset / 16),
+                                            ),
+                                          ),
+                                        if (_printervisibility)
+                                          Positioned(
+                                            left: 838 * scale_offset,
+                                            top: 1988 * scale_offset,
+                                            child: Image.asset(
+                                              printerPath,
+                                              scale: 1 / (scale_offset / 16),
+                                            ),
+                                          ),
+                                        if (_atmvisibility)
+                                          Positioned(
+                                            left: 2500 * scale_offset,
+                                            top: 1725 * scale_offset,
+                                            child: Image.asset(
+                                              atmPath,
+                                              scale: 1 / (scale_offset / 16),
+                                            ),
+                                          ),
+                                        if (_atmvisibility)
+                                          Positioned(
+                                            left: 1120 * scale_offset,
+                                            top: 3217 * scale_offset,
+                                            child: Image.asset(
+                                              atmPath,
+                                              scale: 1 / (scale_offset / 16),
+                                            ),
+                                          ),
+                                        if (_atmvisibility)
+                                          Positioned(
+                                            left: 1469 * scale_offset,
+                                            top: 2956 * scale_offset,
+                                            child: Image.asset(
+                                              atmPath,
+                                              scale: 1 / (scale_offset / 16),
+                                            ),
+                                          ),
+                                        if (_loungevisibility)
+                                          Positioned(
+                                            left: 1011 * scale_offset,
+                                            top: 2870 * scale_offset,
+                                            child: Image.asset(
+                                              loungePath,
+                                              scale: 1 / (scale_offset / 16),
+                                            ),
+                                          ),
+                                        if (_loungevisibility)
+                                          Positioned(
+                                            left: 1258 * scale_offset,
+                                            top: 1735 * scale_offset,
+                                            child: Image.asset(
+                                              loungePath,
+                                              scale: 1 / (scale_offset / 16),
+                                            ),
+                                          ),
                                       ],
                                     ),
                                   ),
